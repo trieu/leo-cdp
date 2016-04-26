eb0a7a9947fc7f1ad113cbeb1c226d0a8f80092b
trieu <tantrieuf31@gmail.com>
2016-04-26 15:28:20 +0700

    JSON schema creative

---
+++ nodejs-test/test-save-creative.js
@@ -0,0 +1,33 @@
+{
+	"id":0,
+	"name":"iTVad-Hyundai-trial-test-2016",
+	"desc":"",
+	"status":2,
+	"adType":1,
+	"prcModel":2,
+	"cost":0,
+	"tBk":200000,
+	"dBk":0,
+	"hBk":0,
+	"score":30,
+	"media":"1005-sd.mp4",
+
+	"runDate":"Apr 26, 2016 12:00:00 AM",
+	"expiredDate":"May 15, 2016 12:00:00 AM",
+		
+	"skip":"00:00:05",
+	"dura":"00:00:29",
+	"cliThr":"",
+	"cliId":0,
+	"cpnId":0,
+	"flId":0,
+	"fqcCap":0,
+	
+	"is3rdAd":false,
+	"vastXml3rd":"",	
+	
+	"discount":0.0,	
+	"tgpms":[101,102,201],
+	"tgpfs":[1,5,4],
+	"tggds":[2]
+}
