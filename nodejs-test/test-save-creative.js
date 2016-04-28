00ce5d3a7b334c8b66cb5a26fa4b2a7209cb1bf3
trieu <tantrieuf31@gmail.com>
2016-04-28 17:14:15 +0700

    test code

--- nodejs-test/test-save-creative.js
+++ nodejs-test/test-save-creative.js
@@ -3,25 +3,25 @@ var request = require('request');
 
 var sampleAdData = 
 {
-	"id":0,
-	"name":"unit-test",
+	"id":188,
+	"name":"FptPlay-TVC-EPL-Arsenal-Norwich City",
 	"desc":"",
 	"status":2,
 	"adType":1,
-	"prcModel":2,
+	"prcModel":5,
 	"cost":0,
-	"tBk":200000,
+	"tBk":0,
 	"dBk":0,
 	"hBk":0,
 	"score":30,
-	"media":"1005-sd.mp4",
+	"media":"187-sd.mp4",
 
-	"runDate":"Apr 26, 2016 12:00:00 AM",
-	"expiredDate":"May 15, 2016 12:00:00 AM",
+	"runDate": "2016-04-20",
+	"expiredDate": "2016-04-30",
 		
 	"skip":"00:00:05",
-	"dura":"00:00:29",
-	"cliThr":"",
+	"dura":"00:00:30",
+	"cliThr":"https://goo.gl/43rlje",
 	"cliId":0,
 	"cpnId":0,
 	"flId":0,
@@ -35,7 +35,8 @@ var sampleAdData =
 	"tgpfs":[1,5,4],
 	"tggds":[2]
 };
-request('http://d1.adsplay.net/ping', function (error, response, body) {
+
+request('http://api.adsplay.net/creative/save/json', function (error, response, body) {
     if (!error && response.statusCode == 200) {
 		console.log(body);
 	}
