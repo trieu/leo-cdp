3ce0440e0d32344303a6207561a410cea1ee1fa8
mith <truonghoangmi@gmail.com>
2016-05-16 11:46:36 +0700

    set auth

--- adsplay-public-admin/application/modules/admin_auth/views/List_User.php
+++ adsplay-public-admin/application/modules/admin_auth/views/List_User.php
@@ -1,13 +1,13 @@
 <div class="col-md-12 col-sm-12 col-xs-12">
     <div class="x_panel">
         <div class="x_title">
-            <h2>List <small>Campaigns</small></h2>
+            <h2>List <small>Users</small></h2>
             <div class="clearfix"></div>
         </div>
         <div class="x_content">
             <?php if($this->ion_auth->in_group('superAdmin')):?>
             <p class="text-muted font-13 m-b-30">
-                <button class="btn btn-success" onclick="add_User()"><i class="glyphicon glyphicon-plus"></i> Add Campaigns</button>
+                <button class="btn btn-success" onclick="add_User()"><i class="glyphicon glyphicon-plus"></i> Add User</button>
             </p>
             <?php endif; ?>
             <table id="datatable-Campaign" class="table table-striped table-bordered dt-responsive nowrap" cellspacing="0" width="100%">
