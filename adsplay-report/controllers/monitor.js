03f7b174afc075f61cf6fe13b5313793ff8c1af9
kimthiendung90 <allgame789@gmail.com>
2016-08-11 14:56:23 +0700

    fix api paytv

--- adsplay-report/controllers/monitor.js
+++ adsplay-report/controllers/monitor.js
@@ -62,7 +62,7 @@ router.get('/inventory-paytv/api/:begin/:end', function(req, res){
     var begin = req.params.begin;
     var end = req.params.end;
     var data = modelUtils.baseModel(req);
-    request(data.site.api_paytv+"/OneTVWS.ashx?method=ITVad_TotalView&begintime="+begin+"&endtime="+end,
+    request(data.site.api_paytv+"/itvads?method=ITVad_TotalView&begintime="+begin+"&endtime="+end,
         function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 res.json(JSON.parse(body));
@@ -81,7 +81,7 @@ router.get('/inventory-paytv-ads', function (req, res) {
 router.get('/inventory-paytv-ads/api/:begin/:end', function(req, res){
     var begin = req.params.begin;
     var end = req.params.end;
-    request(data.site.api_paytv+"/OneTVWS.ashx?method=ITVad_TotalViewAds&begintime="+begin+"&endtime="+end,
+    request(data.site.api_paytv+"/itvads?method=ITVad_TotalView&begintime="+begin+"&endtime="+end,
         function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 res.json(JSON.parse(body));
