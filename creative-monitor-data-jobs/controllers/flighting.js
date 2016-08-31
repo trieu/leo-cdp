c2e49e3947af98617e679f3f1a70c922cf407393
kimthiendung90 <allgame789@gmail.com>
2016-08-31 14:01:03 +0700

    update code

--- creative-monitor-data-jobs/controllers/flighting.js
+++ creative-monitor-data-jobs/controllers/flighting.js
@@ -1,80 +1,118 @@
 var site = require('../configs/site');
-var flighting_daily = require('../models/flighting').flighting_daily;
+var flighting_hourly = require('../models/flighting').flighting_hourly;
+var Sync = require('sync');
 var moment = require('moment');
 var _ = require('underscore');
 var request = require('request');
 var schedule = require('node-schedule');
 
 
-//___ create data 
-// id = id creative , begin <= "running booking" < end 
-// var items = [
-// 	{id: '1052', booking: 0, begin: '8/30/2016, 1:53:33 PM' , end: '8/30/2016, 2:53:33 PM'}
-// ];
-// for (var i in items) {
+//____  create data  with { id = id creative , begin <= "running booking" < end } 
 
-// 	flighting_daily.findOne({id: items[i].id}, function(err, doc){
-
-// 		if(err){
-// 			return console.error(err);
-// 		}
-// 		if(_.isEmpty(doc)){
-// 			var obj = new flighting_daily(items[i]);
-// 			obj.save(function(err, obj){
-// 				if(err){
-// 					return console.error(err);
-// 				}
-// 			});
-// 		}
-// 	});
-// }
-
-flighting_daily.find({}, function(err, doc){
-	if(err){
-		return console.error(err);
+var io_create = function(items, callback){
+	for (var i in items) {
+		flighting_hourly.findOneAndUpdate({id: items[i].id}, items[i], {upsert:true}, function(err, doc){
+			if(err){
+				return console.error(err);
+			}
+		});	
 	}
+	callback();
+};
 
-	if(_.isEmpty(doc)){
-		return console.log('no data !');
-	}
-
-	for(var i in doc){
-		var now = new Date().getTime();
-		var begin = new Date(doc[i].begin).getTime();
-		var end = new Date(doc[i].end).getTime();
-
-		//first set
-		if(now >= begin && now < end){
-			update(doc[i].id, '2');
-		}
-		else{
-			update(doc[i].id, '1');
+//hanlde schedule
+var io_schedule = function(callback){
+	flighting_hourly.find({}, function(err, doc){
+		if(err){
+			return console.error(err);
 		}
 
-		//____auto run schedule job____
+		if(_.isEmpty(doc)){
+			return console.error('no data !');
+		}
 
-		//set auto running
-		set_schedule(doc[i].id, doc[i].begin, '2');
+		for(var i in doc){
+			var now = new Date().getTime();
+			var begin = new Date(doc[i].begin).getTime();
+			var end = new Date(doc[i].end).getTime();
 
-		//set auto pending
-		set_schedule(doc[i].id, doc[i].end, '1');
-	}
+			//first set
+			if(begin >= end){
+				return console.error('data error');
+			}
+			else if(now >= begin && now < end){
+				api_update(doc[i].id, '2');
+			}
+			else{
+				api_update(doc[i].id, '1');
+			}
 
-});
+			//set auto running
+			set_schedule(doc[i].id, doc[i].begin, '2');
+
+			//set auto pending
+			set_schedule(doc[i].id, doc[i].end, '1');
+		}
+		//callback();
+	});
+
+};
 
 var	set_schedule = function(id, time, status){
 	var rule = new schedule.RecurrenceRule();
-	rule.dayOfWeek = [0, new schedule.Range(0, 6)];
-	rule.hour = moment(time).get('hour');
-	rule.minute = moment(time).get('minute');
-	
+			rule.dayOfWeek = [0, new schedule.Range(0, 6)];
+			rule.hour = moment(time).get('hour');
+			rule.minute = moment(time).get('minute');
+
+	//status = running check total booking
 	schedule.scheduleJob(rule, function(){
-		console.log('update : id ' +id+ ' status '+ status);
-		update(id, status);
+		api_update(id, status);
 	});
+
 };
 
-var update = function(id, status){
-	request.patch(site.api_domain + '/api/creatives/' + id).form(JSON.stringify({ status: status }));
-	// console.log(site.api_domain + '/api/creatives/' + id +' ____updated....')
+var api_update = function(id, status){
+	if(status == '2'){
+		api_checkBooking(id, function(data){
+			if(data != null){
+				request.patch(site.api_domain + '/api/creatives/' + id).form(JSON.stringify({ status: status }));
+				console.log('updated : id ' +id+ ' status '+ status);
+			};
+		});
+	}
+	else{
+		request.patch(site.api_domain + '/api/creatives/' + id).form(JSON.stringify({ status: status }));
+		console.log('updated : id ' +id+ ' status '+ status);
+	}
 }
+
+var api_checkBooking = function(id, callback){
+	request(site.api_domain + '/api/creatives/' + id ,function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			if(body.tBk < 0 || body.tBk >= body.totalRevenue ){
+				callback(null);
+			}
+			else{
+				callback(JSON.parse(body));
+			}
+		}
+    });
+};
+
+Sync(function(){
+	var items = [
+		{id: '1064', booking: 0, begin: '8/31/2016, 9:00:00 AM' , end: '8/31/2016, 1:28:00 PM'},
+		{id: '1052', booking: 0, begin: '8/31/2016, 9:00:00 AM' , end: '8/31/2016, 1:27:00 PM'}
+	];
+
+	try {
+		//create 
+        io_create.sync(null, items);
+        io_schedule.sync(null);
+    }
+    catch (e) {
+        console.error(e); // something went wrong 
+    }
+
+});
+
--- creative-monitor-data-jobs/models/flighting.js
+++ creative-monitor-data-jobs/models/flighting.js
@@ -1,25 +1,17 @@
 var dbConfig = require('../configs/database');
 var mongoose = require('mongoose');
-var autoIncrement = require('mongoose-auto-increment');
 
 //setup connect autoIncrement
 var connection = mongoose.createConnection(dbConfig.local);
-autoIncrement.initialize(connection);
 
-var flighting_daily = new mongoose.Schema({
-  id: { type: Number, index: true },
+var flighting_hourly = new mongoose.Schema({
+  id: { type: Number, require: true, unique: true},
   booking: { type: Number , default: 0},
   begin: {type: Date},
   end: {type: Date}
 });
 
-flighting_daily.plugin(autoIncrement.plugin, {
-    model: 'flighting_daily',
-    field: 'id',
-    startAt: 1000
-});
-
 module.exports = {
-	flighting_daily: mongoose.model('flighting_daily', flighting_daily, 'flighting_daily')
+	flighting_hourly: mongoose.model('flighting_hourly', flighting_hourly, 'flighting_hourly')
 }
 
