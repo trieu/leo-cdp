a464687172a4ea11720339206e9aa3266f27663c
dev <allgame789@gmail.com>
2016-11-18 16:51:30 +0700

    update code placement & fix page Total Completed View

--- adsplay-report/public/js/business-domains/placement-handler.js
+++ adsplay-report/public/js/business-domains/placement-handler.js
@@ -14,6 +14,7 @@ $(window).load(function() {
 				$.each(result, function (i, item) {
 					$("#ads-publisher").append('<option value="'+item._id+'">'+item.name+'</option>');
 				});
+				responsiveCheck();
 			}
 		});
 	},100);
@@ -43,6 +44,14 @@ $(window).load(function() {
 			}
 		}
 		return false;
+	}
+
+	function responsiveCheck(){
+		$(".plt_w, .plt_h").each(function(){
+			if($(this).text() == "0"){
+				$(this).text("100%");
+			}
+		});
 	}
 
 	$("#table-placement").DataTable({"pageLength": 25});
@@ -182,6 +191,7 @@ $(window).load(function() {
 				else{
 					$('#plt_container').append(plt_print(result));
 				}
+				responsiveCheck();
 
 				$('#modal-placement').modal('hide');
 				
