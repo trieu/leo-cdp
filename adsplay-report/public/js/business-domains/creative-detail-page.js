1a415baa46b22bc6c9d374323f7cda1cecfc7907
dev <allgame789@gmail.com>
2016-12-12 16:41:22 +0700

    update show video youtube detail page

--- adsplay-report/public/js/business-domains/creative-detail-page.js
+++ adsplay-report/public/js/business-domains/creative-detail-page.js
@@ -16,6 +16,20 @@ $(document).ready(function () {
         }
     });
 
+    // video is youtube
+    var video_ads = $("#video_ads").hide();
+    var parent_video = $("#video_ads").parent();
+    var src = video_ads.find("source").attr("src");
+    var arr = src.split("https://");
+    for(var i in arr){
+        var checkLink = checkLinkYoutube(arr[i]);
+        if(checkLink != "error"){
+            var link = "https://www.youtube.com/embed/"+checkLink;
+            parent_video.html('<iframe width="100%" height="300" src="'+link+'" frameborder="0" allowfullscreen></iframe>');
+        }
+    }
+
+
 });
 
 //format time for Chart
