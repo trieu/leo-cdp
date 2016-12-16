6919248168a0bb26469b4f598386a60925e18c56
dev <allgame789@gmail.com>
2016-12-16 17:06:51 +0700

    Create Inventory

--- adsplay-report/gulpfile.js
+++ adsplay-report/gulpfile.js
@@ -4,14 +4,14 @@ var concat = require('gulp-concat');
 var cleanCss = require('gulp-clean-css');
 
 gulp.task('js-min', function() {
-	return gulp.src(['public/js/*.js','!public/js/jquery.js','!public/js/jquery-ui.min.js','!public/js/moment.min.js','!public/js/js-min.js'])
+	return gulp.src(['public/js/*.js','!public/js/jquery.js','!public/js/jquery-ui.min.js','!public/js/moment.min.js','!public/js/js-min.js',,'!public/js/jquery.multi-select.js'])
 			.pipe(concat('js-min.js'))
 			.pipe(uglify({mangle: false}))
 			.pipe(gulp.dest('public/js'))
 });
 
 gulp.task('css-min', function() {
-	return gulp.src(['public/css/*.css','!public/css/css-min.css'])
+	return gulp.src(['public/css/*.css','!public/css/css-min.css','!public/css/multi-select.css'])
 			.pipe(concat('css-min.css'))
 			.pipe(cleanCss())
 			.pipe(gulp.dest('public/css'));
