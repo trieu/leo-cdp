358dd54a19401857b4dc0ab09635747609956e7a
dev <allgame789@gmail.com>
2017-01-05 15:31:53 +0700

    update code

--- analytics-dashboard/src/data.js
+++ analytics-dashboard/src/data.js
@@ -12,7 +12,6 @@ const data = {
   menus: [
     { text: 'DashBoard', icon: <Assessment/>, link: '/dashboard' },
     { text: 'Form Page', icon: <Web/>, link: '/form' },
-    { text: 'Table Page', icon: <GridOn/>, link: '/table' },
     { text: 'Login Page', icon: <PermIdentity/>, link: '/login' }
   ],
   tablePage: {
--- analytics-dashboard/src/routes.js
+++ analytics-dashboard/src/routes.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './layouts/App';
+import DashBoard from './pages/DashBoard';
 import FormPage from './pages/FormPage';
 import NotFoundPage from './pages/NotFoundPage';
 
 export default (
   <Route>
     <Route path="/" component={App}>
-      <IndexRoute component={FormPage}/>
+      <IndexRoute component={DashBoard}/>
+      <Route path="dashboard" component={DashBoard}/>
       <Route path="form" component={FormPage}/>
       <Route path="*" component={NotFoundPage}/>
     </Route>
