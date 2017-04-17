09a69322c177bb029c9b26ba294f84f7eda08b42
trieu <tantrieuf31@gmail.com>
2017-04-17 18:30:50 +0700

    demo ads

--- adsplay-report/models/creative.js
+++ adsplay-report/models/creative.js
@@ -83,7 +83,9 @@ exports.list = function (url, data, callback) {
                 else if (data.ssid === 1011 && (adName.indexOf('vtc') >= 0 )) {
                     filteredList.push(crt);
                 }
-
+                else if (data.ssid === 1013 && (adName.indexOf('danet') >= 0 )) {
+                    filteredList.push(crt);
+                }
             });
 
 			callback(null, filteredList);
