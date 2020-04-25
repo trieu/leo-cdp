c9cbd818e2aa8ff2444da69ee7975e0af3472be7
Trieu Nguyen <trieunt@blueseed.tv>
2020-04-25 14:20:03 +0700

    model

--- core-leo-cms/src/main/java/leotech/cms/query/ContentQueryUtil.java
+++ core-leo-cms/src/main/java/leotech/cms/query/ContentQueryUtil.java
@@ -3,12 +3,9 @@ package leotech.cms.query;
 import java.util.Arrays;
 import java.util.List;
 
-import leotech.cms.dao.PostDaoUtil;
 import leotech.cms.model.Post;
-import leotech.cms.model.common.MediaNode;
 import redis.clients.jedis.ShardedJedisPool;
 import rfx.core.configs.RedisConfigs;
-import rfx.core.configs.WorkerConfigs;
 
 /**
  * Ad Data Query Util, data from MongoDB, for delivery
@@ -26,16 +23,16 @@ public class ContentQueryUtil {
  
     public static List<Post> queryPost(ContentQuery q) {
 	//TODO
-//	ContentNode d = new Post("Secret Garden Songs","https://www.youtube.com/embed/PaoPzDW2m2A",1);
-//	ContentNode d2 = new Post("HongKong1 | OFFICIAL MV | Nguyễn Trọng Tài x San Ji x Double X","https://www.youtube.com/watch?v=t7tZFq29lis",1);
-//	ContentNode d5 = new Post("Chuyện Tình Lướt Qua - Hong Kong 1 - Nguyễn Trọng Tài (Nguyễn Anh Cover)","https://www.youtube.com/embed/82D9N6ah2tg",1);
-//	
-//	ContentNode d3 = new Post("The Greatest Showman Full Soundtrack Complete OST FULL OST","https://www.facebook.com/xemgiday.tv/videos/1764726960495574/",1);
-//	ContentNode d4 = new Post("Mind Of | Beautiful Chill Mix","https://www.youtube.com/watch?v=YUIgPRQNjEA",1);
-//	
-//	return Arrays.asList(d,d2,d3,d4,d5);
+	Post d = new Post("Secret Garden Songs","https://www.youtube.com/embed/PaoPzDW2m2A",1,"");
+	Post d2 = new Post("HongKong1 | OFFICIAL MV | Nguyễn Trọng Tài x San Ji x Double X","https://www.youtube.com/watch?v=t7tZFq29lis",1,"");
+	Post d5 = new Post("Chuyện Tình Lướt Qua - Hong Kong 1 - Nguyễn Trọng Tài (Nguyễn Anh Cover)","https://www.youtube.com/embed/82D9N6ah2tg",1,"");
 	
-	List<Post> posts = PostDaoUtil.listByNetwork(2, 0, 50);
+	Post d3 = new Post("The Greatest Showman Full Soundtrack Complete OST FULL OST","https://www.facebook.com/xemgiday.tv/videos/1764726960495574/",1,"");
+	Post d4 = new Post("Mind Of | Beautiful Chill Mix","https://www.youtube.com/watch?v=YUIgPRQNjEA",1,"");
+	
+	List<Post> posts = Arrays.asList(d,d2,d3,d4,d5);
+	
+	//List<Post> posts = PostDaoUtil.listByNetwork(2, 0, 50);
 	return posts;
     }
 
