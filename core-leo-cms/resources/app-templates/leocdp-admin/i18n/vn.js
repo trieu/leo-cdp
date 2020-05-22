f3d0e456700072b6f03b47706e58d1d2582aa84d
Trieu Nguyen <trieunt@blueseed.tv>
2020-05-22 19:38:27 +0700

    profile view

--- core-leo-cms/resources/app-templates/leocdp-admin/i18n/vn.js
+++ core-leo-cms/resources/app-templates/leocdp-admin/i18n/vn.js
@@ -1,5 +1,7 @@
 window.i18nLeoAdminData = window.i18nLeoAdminData || function () {
 	var data = {};
+	
+	//customer journey map view
 	data["target_customer_name"] = "Tên khách hàng mục tiêu";
 	data["persona_attributes"] = "1) Thuộc tính cá nhân";
 	data["persona_expectations"] = "2) Nhu cầu và vấn đề của khách hàng"; // Pain-points, expectations and problems 
@@ -9,5 +11,17 @@ window.i18nLeoAdminData = window.i18nLeoAdminData || function () {
 	data["persona_touchpoints"] = "6) Điểm tiếp xúc thông tin và trải nghiệm";
 	data["conversion_actions"] = "7) Những hành động của khách hàng tiềm năng (Lead)";
 	data["key_business_action"] = "8) Hành động chính để trở thành khách hàng";
+	
+	//profile 360 analytics view
+	
+	data["profile_full_name"] = "Tên khách hàng";
+	data["profile_attributes"] = "Thuộc tính cá nhân";
+	data["profile_expectations"] = "Nhu cầu và vấn đề của khách hàng"; 
+	data["profile_interests"] = "Sở thích cá nhân và động lực hành động"; 
+	data["profile_recommended_ideas"] = "Ý tưởng tiếp cận do hệ thống gợi ý"; 
+	data["profile_recommended_solutions"] = "Sản phẩm và dịch vụ do hệ thống gợi ý"; 
+	data["profile_touchpoints"] = "Các điểm đã tiếp xúc thông tin và trải nghiệm";
+	data["profile_actions"] = "Những hành động của khách hàng";
+	
 	return data;
 }
