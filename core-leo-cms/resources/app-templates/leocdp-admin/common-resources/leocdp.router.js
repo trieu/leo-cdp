543286383a3ff9869fc7ec4c3b769059630ccc9e
Trieu Nguyen <trieunt@blueseed.tv>
2020-05-25 11:19:01 +0700

    customer 360 analytics

--- core-leo-cms/resources/app-templates/leocdp-admin/common-resources/leocdp.router.js
+++ core-leo-cms/resources/app-templates/leocdp-admin/common-resources/leocdp.router.js
@@ -13,113 +13,138 @@ function loadJourneyMapStudio() {
     });
 }
 
+function loadProductCatalogManagement(){
+	loadView('/view/hubs/in-development.html?admin=1', pageDomSelector, function () {
+		//TODO
+    });
+}
+
+function loadServiceCatalogManagement(){
+	loadView('/view/hubs/in-development.html?admin=1', pageDomSelector, function () {
+		//TODO
+    });
+}
+
+function loadBusinessHubApiManagement(){
+	loadView('/view/hubs/in-development.html?admin=1', pageDomSelector, function () {
+		//TODO
+    });
+}
+
 //###################### Content Hub ######################
 
 //------------ Page Controllers ---------------------
 
 function pageEditor(id, categoryKey) {
- console.log('pageEditor');
- document.title = 'Page Editor';
-
- loadView('/view/page-editor.html?admin=1', pageDomSelector, function () {
-     if (id) {
-         console.log('edit page ' + id);
-         loadDataPageEditor({
-             'pageId': id,
-             'categoryKey': categoryKey
-         })
-     } else {
-         console.log('new page');
-         loadDataPageEditor({
-             'pageId': "",
-             'categoryKey': categoryKey
-         })
-     }
- });
+	 console.log('pageEditor');
+	 document.title = 'Page Editor';
+	
+	 loadView('/view/page-editor.html?admin=1', pageDomSelector, function () {
+	     if (id) {
+	         console.log('edit page ' + id);
+	         loadDataPageEditor({
+	             'pageId': id,
+	             'categoryKey': categoryKey
+	         })
+	     } else {
+	         console.log('new page');
+	         loadDataPageEditor({
+	             'pageId': "",
+	             'categoryKey': categoryKey
+	         })
+	     }
+	 });
 }
 
 function pageInfo(pageId, pageName, categoryKey) {
- document.title = 'Page Info: ' + decodeURIComponent(pageName);
- console.log('pageInfo');
- loadView('/view/page-info-with-posts.html?admin=1', pageDomSelector, function () {
-     pageInfoCallback(pageId, pageName, categoryKey);
- });
+	 document.title = 'Page Info: ' + decodeURIComponent(pageName);
+	 console.log('pageInfo');
+	 loadView('/view/page-info-with-posts.html?admin=1', pageDomSelector, function () {
+	     pageInfoCallback(pageId, pageName, categoryKey);
+	 });
 }
 
 function deletePage() {
- $('#delete_callback').val('');
- $('#confirmDeleteDialog').modal({
-     focus: true
- });
- if (pageModel) {
-     var callback = "deletePage" + pageModel.id;
-     window[callback] = function () {
-         var urlStr = baseAdminApi + '/page/delete';
-         var params = {
-             'pageId': pageModel.id
-         };
-         LeoCmsApiUtil.callPostAdminApi(urlStr, params, function (json) {
-             if (json.httpCode === 0 && json.errorMessage === '') {
-                 if (json.data) {
-                     location.href = '/admin';
-                 }
-             }
-         });
-     }
-     $('#delete_callback').val(callback);
-     $('#deletedInfoTitle').html(pageModel.title);
-     $('#deletedInfoMsg').html('Do you want to delete this page ?');
- }
+	 $('#delete_callback').val('');
+	 $('#confirmDeleteDialog').modal({
+	     focus: true
+	 });
+	 if (pageModel) {
+	     var callback = "deletePage" + pageModel.id;
+	     window[callback] = function () {
+	         var urlStr = baseAdminApi + '/page/delete';
+	         var params = {
+	             'pageId': pageModel.id
+	         };
+	         LeoCmsApiUtil.callPostAdminApi(urlStr, params, function (json) {
+	             if (json.httpCode === 0 && json.errorMessage === '') {
+	                 if (json.data) {
+	                     location.href = '/admin';
+	                 }
+	             }
+	         });
+	     }
+	     $('#delete_callback').val(callback);
+	     $('#deletedInfoTitle').html(pageModel.title);
+	     $('#deletedInfoMsg').html('Do you want to delete this page ?');
+	 }
 }
 
 //------------ Post Controllers ---------------------
 
 function postEditor(id, pageId, categoryKey) {
- console.log('postEditor' + id);
- loadView('/view/post/post-editor.html?admin=1', pageDomSelector, function () {
-     console.log('edit post ' + id);
-     initPostEditor({
-         'postId': id,
-         'pageId': pageId,
-         'categoryKey': categoryKey
-     });
- });
+	 console.log('postEditor' + id);
+	 loadView('/view/post/post-editor.html?admin=1', pageDomSelector, function () {
+	     console.log('edit post ' + id);
+	     initPostEditor({
+	         'postId': id,
+	         'pageId': pageId,
+	         'categoryKey': categoryKey
+	     });
+	 });
 }
 
 function postInfo(id) {
- if (id) {
-     loadView('/view/post/post-info.html?admin=1', pageDomSelector, function () {
-     	initPostInfoView({
-             postId: id
-         });
-     });
- }
+	 if (id) {
+	     loadView('/view/post/post-info.html?admin=1', pageDomSelector, function () {
+	     	initPostInfoView({
+	             postId: id
+	         });
+	     });
+	 }
 }
 
 function deletePost() {
- $('#delete_callback').val('');
- $('#confirmDeleteDialog').modal({
-     focus: true
- });
- if (postModel) {
-     var callback = "deletePost" + postModel.id;
-     window[callback] = function () {
-         var urlStr = baseAdminApi + '/post/delete';
-         var params = {
-             'postId': postModel.id
-         };
-         LeoCmsApiUtil.callPostAdminApi(urlStr, params, function (json) {
-             if (json.httpCode === 0 && json.errorMessage === '') {
-                 if (json.data) {
-                     location.href = '/admin';
-                 }
-             }
-         });
-     }
-     $('#delete_callback').val(callback);
-     $('#deletedInfoTitle').html(postModel.title);
-     $('#deletedInfoMsg').html('Do you want to delete this post ?');
- }
+	 $('#delete_callback').val('');
+	 $('#confirmDeleteDialog').modal({
+	     focus: true
+	 });
+	 if (postModel) {
+	     var callback = "deletePost" + postModel.id;
+	     window[callback] = function () {
+	         var urlStr = baseAdminApi + '/post/delete';
+	         var params = {
+	             'postId': postModel.id
+	         };
+	         LeoCmsApiUtil.callPostAdminApi(urlStr, params, function (json) {
+	             if (json.httpCode === 0 && json.errorMessage === '') {
+	                 if (json.data) {
+	                     location.href = '/admin';
+	                 }
+	             }
+	         });
+	     }
+	     $('#delete_callback').val(callback);
+	     $('#deletedInfoTitle').html(postModel.title);
+	     $('#deletedInfoMsg').html('Do you want to delete this post ?');
+	 }
+}
+
+
+function loadContentHubApiManagement(){
+	loadView('/view/hubs/in-development.html?admin=1', pageDomSelector, function () {
+        //TODO
+    });
 }
 
 //------------ Category Controllers ---------------------
@@ -176,9 +201,10 @@ function loadAudienceSegmentation() {
     });
 }
 
-function loadAudienceHubApiManagement() {
-    loadView('/view/hubs/audience/audience-hub-api-configs.html?admin=1', pageDomSelector, function () {
-    	initSalesDashboard();
+
+function loadAudienceHubApiManagement(){
+	loadView('/view/hubs/in-development.html?admin=1', pageDomSelector, function () {
+        //TODO
     });
 }
 
@@ -195,6 +221,8 @@ function loadProfileDataEditor() {
     	initProfileDataEditor();
     });
 }
+
+
 
 // ------------ Report Analytics Controllers ---------------------
 
@@ -213,3 +241,4 @@ function loadUserReport() {
         loadDataUserAnalytics();
     });
 }
+
